49ebf1645afd02211e1a82311d157da43bf79b2e
Rashmi-ms <rashmims56@gmail.com>
2021-06-08 09:49:14 +0530

    payment-service

---
+++ payment-service/src/main/java/com/flightbookingsystem/paymentservice/controller/PaymentController.java
@@ -0,0 +1,14 @@
+package com.flightbookingsystem.paymentservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+	
+	@GetMapping("/hellopayment")
+	public String test() {
+		return "Hello Payment Service";
+	}
+
+}
