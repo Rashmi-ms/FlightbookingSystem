01814e01cc46fb2cea5bf41d98c1fd8f3f37d0a2
Rashmi-ms <rashmims56@gmail.com>
2021-06-10 22:20:17 +0530

    booking-service

---
+++ booking-service/src/main/java/com/flightbookingsystem/bookingservice/repository/BookingRepository.java
@@ -0,0 +1,23 @@
+package com.flightbookingsystem.bookingservice.repository;
+
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+
+import com.flightbookingsystem.bookingservice.model.Booking;
+
+@Repository
+public interface BookingRepository extends MongoRepository<Booking, String>{
+
+	Optional<Booking> findById(int id);
+
+	Booking deleteById(int id);
+
+	
+
+	
+
+}
