4b6987a54a015fbd585c4606aab6b4b849daacfe
Rashmi-ms <rashmims56@gmail.com>
2021-06-11 21:59:16 +0530

    Payment-gateway implemented

--- payment-service/src/main/java/com/flightbookingsystem/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/flightbookingsystem/paymentservice/PaymentServiceApplication.java
@@ -2,9 +2,19 @@ package com.flightbookingsystem.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.flightbookingsystem.paymentservice.model.PaytmDetail;
+
+
 
 @SpringBootApplication
 public class PaymentServiceApplication {
+	
+	@Bean
+	public PaytmDetail getpaymentdetails() {
+		return new PaytmDetail();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentServiceApplication.class, args);
