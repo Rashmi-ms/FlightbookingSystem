214f0f1305ae92d1a5c3e7ea8eb0f91edfb99d5d
Rashmi-ms <rashmims56@gmail.com>
2021-06-15 21:56:41 +0530

    zuul-gateway updated

---
+++ zuul-gateway/src/main/java/com/flightbookingsystem/zuulgateway/filters/ErrorFilter.java
@@ -0,0 +1,45 @@
+package com.flightbookingsystem.zuulgateway.filters;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Configuration
+public class ErrorFilter extends ZuulFilter {
+	
+	private static Logger log = LoggerFactory.getLogger(ErrorFilter.class);
+
+	@Override
+	public boolean shouldFilter() {
+		
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+        HttpServletResponse response = RequestContext.getCurrentContext().getResponse();
+	    
+	    log.info("ErrorFilter: " + String.format("response status is %d", response.getStatus()));
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		
+		return "error";
+	}
+
+	@Override
+	public int filterOrder() {
+		
+		return 1;
+	}
+    
+}
