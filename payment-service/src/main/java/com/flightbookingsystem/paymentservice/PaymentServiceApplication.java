f67bd14c22adf6f47afb71f2b3e231c22d2f7831
Rashmi-ms <rashmims56@gmail.com>
2021-06-15 22:00:47 +0530

    payment-service

--- payment-service/src/main/java/com/flightbookingsystem/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/flightbookingsystem/paymentservice/PaymentServiceApplication.java
@@ -2,12 +2,13 @@ package com.flightbookingsystem.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import com.flightbookingsystem.paymentservice.model.PaytmDetail;
 
 
-
+@EnableEurekaClient
 @SpringBootApplication
 public class PaymentServiceApplication {
 	
